c5864f143e3d8f30d533f021e760b338887051c5
Patrick Clary <pclary@outlook.com>
2015-05-15 19:14:15 -0700

    Removed unnecessary things

--- Teensymouse2015/Globals.cpp
+++ Teensymouse2015/Globals.cpp
@@ -2,55 +2,20 @@
 #include <Arduino.h>
 
 
-State state = {0.f, 0.f, pi*0.5f};
-State target = {0.f, 0.f, pi*0.5f};
 Node currentCell = {0, 0};
-Node targetCell = {0, 0};
-Node prevTargetCell = {0, 0};
-int lCount = 0;
-int rCount = 0;
-LowPass drdt;
-LowPass dfdt;
-LowPass dldt;
-LowPass dthdt;
-LowPass dsdt;
-float thgoal = 0.f;
-float therr = 0.f;
-float thctrl = 0.f;
-float speed = 0.f;
-float maxSpeed = 0.2f;
-float ctheta = 0.03f;
-float cside = 0.1f;
-float cfront = 0.1f;
-float targetDist = 0.f;
 Direction direction = Direction::undefined;
-bool inDeadband = false;
-Maze<16, 16> maze;
-bool manualSlow = false;
-float thetakp = 0.4f;
-float thetakd = 0.01f;
-float thctrlmax = 0.2f;
-
-Button switch1;
-Button switch2;
-Button button1;
-Button button2;
-
-Wheel leftWheel(motorLR, motorLF, encoderL1, encoderL2, dt, wheelCirc, ppr);
-Wheel rightWheel(motorRR, motorRF, encoderR1, encoderR2, dt, wheelCirc, ppr);
-
+float speed = 0.5f;
+Motor leftMotor;
+Motor rightMotor;
 VL6180X rightSensor(0);
 VL6180X frontSensor(1);
 VL6180X leftSensor(2);
+LowPass drdt;
+LowPass dfdt;
+LowPass dldt;
+Maze<16, 16> maze;
 
-
-void rled(bool on)
+void led(bool on)
 {
-    digitalWriteFast(led1Pin, on); 
-}
-
-
-void lled(bool on)
-{
-    digitalWriteFast(led2Pin, on);
+    digitalWriteFast(ledPin, on);
 }
--- Teensymouse2015/Globals.h
+++ Teensymouse2015/Globals.h
@@ -1,24 +1,18 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-#include "Wheel.h"
 #include "vl6180x.h"
+#include "drv8833.cpp"
 #include "LowPass.h"
-#include "PIDController.h"
 #include "BFS.h"
 #include "Maze.h"
-#include "Button.h"
 
 //Teensy pinout
-const int led1Pin = 13;
+const int ledPin = 13;
 const int motorRR = 5; // right reverse
 const int motorRF = 6; // right forward motor pin
 const int motorLR = 3; // left forward
 const int motorLF = 4; // left reverse
-
-const unsigned int controlFreq = 200; // Hz
-const unsigned int controlPeriodUs = 1000000 / controlFreq;
-const float dt = 1.f / controlFreq;
 
 const float wheelCirc = 0.1f;
 const float wheelBase = 0.082f;
@@ -35,13 +29,6 @@ const unsigned int sensorFreq = 50; // Hz
 const unsigned int sensorPeriodUs = 1000000 / sensorFreq;
 const float dtsensor = 1.f / sensorFreq;
 
-struct State
-{
-    float x;
-    float y;
-    float theta;
-};
-
 enum class Direction
 {
     undefined,
@@ -51,49 +38,20 @@ enum class Direction
     jneg
 };
 
-extern State state;
-extern State target;
 extern Node currentCell;
-extern Node targetCell;
-extern Node prevTargetCell;
-extern int lCount;
-extern int rCount;
-extern LowPass drdt;
-extern LowPass dfdt;
-extern LowPass dldt;
-extern LowPass dthdt;
-extern LowPass dsdt;
-extern float thgoal;
-extern float therr;
-extern float thctrl;
-extern float speed;
-extern float maxSpeed;
-extern float ctheta;
-extern float cside;
-extern float cfront;
-extern float targetDist;
 extern Direction direction;
-extern bool inDeadband;
-extern Maze<16, 16> maze;
-extern bool manualSlow;
-extern float thetakp;
-extern float thetakd;
-extern float thctrlmax;
-
-extern Button switch1;
-extern Button switch2;
-extern Button button1;
-extern Button button2;
-
-extern Wheel leftWheel;
-extern Wheel rightWheel;
-
+extern float speed;
+extern Motor leftMotor;
+extern Motor rightMotor;
 extern VL6180X rightSensor;
 extern VL6180X frontSensor;
 extern VL6180X leftSensor;
+extern LowPass drdt;
+extern LowPass dfdt;
+extern LowPass dldt;
+extern Maze<16, 16> maze;
 
-void rled(bool on);
-void lled(bool on);
+void led(bool on);
 
 
 #endif // GLOBALS_H
